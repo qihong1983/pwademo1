827813c7b3b839df7ba88c020ea4c6bcd352fa4f
bjqihong <bjqihong@corp.netease.com>
2018-03-04 21:12:00 +0800

    aa

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ window.addEventListener('load', e => {
 
 	if ('serviceWorker' in navigator) {
 		try {
-			navigator.serviceWorker.register('sw.js');
+			navigator.serviceWorker.register('/sw.js');
 			console.log(`SW registered`);
 		} catch (error) {
 			console.log(`SW reg failed`);
